3b21ba525c1b287115caa8a345796944dfb9eefa
ONEONUORA <monweebube44@gmail.com>
2024-03-20 07:21:27 -0700

    AirBnB_clone_v2

---
+++ 100-clean_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A module for web application deployment with Fabric."""
+import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
+
+
+env.hosts = ["34.73.0.174", "35.196.78.105"]
+"""The list of host server IP addresses."""
+
+
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+
